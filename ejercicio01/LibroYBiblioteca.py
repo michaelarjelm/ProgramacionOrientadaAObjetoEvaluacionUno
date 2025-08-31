ffaa8cf6366fb967b5c348b90b59c6c596a2cb75
Javi Lasseube <javiera.lasseube@outlook.com>
2025-08-30 23:37:19 -0400

    Cambios 30/08 ejercicio 1

--- ejercicio01/LibroYBiblioteca.py
+++ ejercicio01/LibroYBiblioteca.py
@@ -1,3 +1,5 @@
+#Construimos la clase Libro
+
 class Libro:
     def __init__ (self, titulo, autor, copias):
         self.titulo = titulo
@@ -5,4 +7,23 @@ class Libro:
         self.copias = int(copias)
 
     def consultar(self):
-        return (f"{self.titulo}, {self.autor}. Número de copias disponibles: {self.copias}.")
+        print(f"'{self.titulo}', {self.autor}. Número de copias disponibles: {self.copias}.")
+
+#Construimos la clase Biblioteca
+
+class Biblioteca:
+    def __init__(self,libro:Libro):
+        self.listado = []
+
+    def agregarLibro(self, libro:Libro):
+        self.listado.append(libro)
+        print(f"¡Has agregado '{libro}' a tu biblioteca!")
+        
+    
+    def prestarLibro(self, libro:Libro):
+        for libro in self.listado:
+            if libro.copias >= 1:
+                # libro.copias -1
+                print(f"Has retirado {libro.titulo}")
+            else:
+                 print("No existen copias disponibles para retirar :(") 
--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from ejercicio01.LibroYBiblioteca import Libro
 
-libroUno = Libro("Orgullo y prejuicio", "Jane Austen", 1)
+libroUno = Libro("Orgullo y prejuicio", "Jane Austen", 4)
 libroDos = Libro("Twilight","Stephenie Meyer", 2)
-libroTres = Libro("El Resplandor", "Stephen King", 1)
+libroTres = Libro("El Resplandor", "Stephen King", 0)
 
-libroUno.consultar()
+#libroUno.consultar()
+#libroDos.consultar()
+#libroTres.consultar()
+
+from ejercicio01.LibroYBiblioteca import Biblioteca
+
+biblioteca = Biblioteca()
+biblioteca.agregarLibro(libroDos)
+biblioteca.agregarLibro(libroUno)
+biblioteca.agregarLibro(libroTres)
+
+biblioteca.prestarLibro(libroTres)
