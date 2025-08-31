6dee94b7e5eb873b40c1dd8bc8c016f11767e500
Alondra Gonzalez <alondra.go15@gmail.com>
2025-08-31 17:27:02 -0400

    usuario

--- Ejercicio6/auth.py
+++ Ejercicio6/auth.py
@@ -1,3 +1,9 @@
 # Ejercicio 6 — Usuario y Autenticación simple
 # Crea una clase Usuario con nombre de usuario y contraseña. Crea una clase Auth que permita
 # registrar usuarios y hacer login validando credenciales.
+
+class Usuario:
+    def __init__(self, nombre, contraseña):
+        self.nombre = nombre
+        self.contraseña = contraseña
+
