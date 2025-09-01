07f74895cd0cb288f1b0b1c66416c682c396d6c8
Emilia.Valenzuela <emii.valenzuelaa@gmail.com>
2025-08-31 21:03:49 -0400

    archivo modificado

---
+++ desarrollo_de_prueba.py
@@ -0,0 +1,30 @@
+import mach
+
+#ejercicio 1 - libro y biblioteca#
+
+
+#ejercicio 2 - alumno y curso#
+
+#ejercicio 3 - pedido e item#
+
+#ejercicio 4 - sensor y mediciones#
+
+#ejercicio 5 - pelicula y catalogo#
+
+#ejercicio 6 - usuario y autenticacion simple#
+
+#ejercicio 7 - agenda y contacto#
+
+#ejercicio 8 - restaurante, mesa y reserva#
+
+#ejercicio 9 - carrito con descuento#
+
+#ejercicio 10 - estudiante y nota#
+
+#ejercicio 11 - empleado y empresa#
+
+#ejercicio 12 - banco y cuentas#
+
+#ejercicio 13 - veterinaria y mascotas#
+
+#ejercicio 14 - examen y preguntas#
---
+++ ejercicio 1 - libro y biblioteca/desarrollo.py
@@ -0,0 +1,29 @@
+
+#desarrollo 1#
+
+
+class Libro:
+
+ def __init__(self, titulo, autor, copias):
+        self.titulo = titulo
+        self.autor = autor
+        self.copias = int (copias)
+
+def consultar(self):
+        print(f"{self.titulo}),{self.autor}. Numero de copias disponibles: {self.copias}.")
+
+
+class Biblioteca:
+    def __init__(self):
+                self.listado = []
+
+def agregarLibro(self, libro: libro):
+    print(f"Felicidades, agregaste el libro {Libro.titulo} a tu biblioteca") 
+
+def prestarLibro(self, libro:Libro): 
+       for Libro in self.listado:
+         if Libro.copias >= 1: 
+            Libro.copias -1
+            print(f"has retirado{self.titulo}")
+         else: 
+             print("no existen copias disponibles de prestamo")    
